97c18e53d171390576df7ddbb0a98878beb1b2d1
Ellie <el@horse64.org>
2023-08-10 17:24:48 +0200

    Make audio test actually open file and add an audio test file
    DCO-1.1-Signed-off-by: Ellie <el@horse64.org>

--- examples/example_audio.c
+++ examples/example_audio.c
@@ -1,6 +1,7 @@
 
 //#define DEBUG_SPEW3D_AUDIO_DECODE
 //#define DEBUG_SPEW3D_AUDIO_DECODE_DATA
+#define DEBUG_SPEW3D_AUDIO_DECODE_RESAMPLE
 #define DEBUG_SPEW3D_AUDIO_SINK
 #define DEBUG_SPEW3D_AUDIO_SINK_DATA
 
@@ -16,21 +17,30 @@
 
 int main(int argc, const char **argv) {
     printf("Initializing Spew3D audio pipeline\n");
-    spew3d_audio_sink *sink = spew3d_audio_sink_CreateStereoOutput(48000);
+    spew3d_audio_sink *sink = spew3d_audio_sink_CreateStereoOutput(44100);
     if (!sink) {
         fprintf(stderr, "Failed to crate sink\n");
         return 1;
     }
     printf("Opening file to directly feed into output sink\n");
-    /*s3daudiodecoder *decoder = audiodecoder_NewFromFile(
-        "test.mp3"
+    int use_parent_file = 0;
+    {
+        int file_exists_in_cwd = 0;
+        if (!spew3d_fs_TargetExists("audiotest.mp3", &file_exists_in_cwd)) {
+            fprintf(stderr, "Failed to find audio file due to I/O error\n");
+            return 1;
+        }
+        use_parent_file = !file_exists_in_cwd;
+    }
+    s3daudiodecoder *decoder = audiodecoder_NewFromFile(
+        (use_parent_file ? "../audiotest.mp3" : "audiotest.mp3")
     );
     if (!decoder || !spew3d_audio_sink_FeedFromDecoder(
             sink, decoder
             )) {
         fprintf(stderr, "Failed to set up decoded file\n");
         return 1;
-    }*/
+    }
 
     printf("Entering main loop\n");
     int notquit = 1;
